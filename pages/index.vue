73de56a61b504562fea68506808ff1d28bdf43c9
Sébastien LeBlanc <sebastien.leblanc@libeo.com>
2019-01-21 09:46:33 -0500

    [FIX] Default template

--- pages/index.vue
+++ pages/index.vue
@@ -2,85 +2,38 @@
     <div class="hello">
         <img src="@/assets/img/logo.png" />
         <h1>{{ msg }}</h1>
-        <h2>
-            Essential Links
-        </h2>
+        <h2>Essential Links</h2>
         <ul>
             <li>
-                <a
-                    href="https://vuejs.org"
-                    target="_blank"
-                >
-                    Core Docs
-                </a>
+                <a href="https://vuejs.org" target="_blank">Core Docs</a>
             </li>
             <li>
-                <a
-                    href="https://forum.vuejs.org"
-                    target="_blank"
-                >
-                    Forum
-                </a>
+                <a href="https://forum.vuejs.org" target="_blank">Forum</a>
             </li>
             <li>
-                <a
-                    href="https://chat.vuejs.org"
-                    target="_blank"
-                >
-                    Community Chat
-                </a>
+                <a href="https://chat.vuejs.org" target="_blank">Community Chat</a>
             </li>
             <li>
-                <a
-                    href="https://twitter.com/vuejs"
-                    target="_blank"
-                >
-                    Twitter
-                </a>
+                <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
             </li>
             <br />
             <li>
-                <a
-                    href="http://vuejs-templates.github.io/webpack/"
-                    target="_blank"
-                >
-                    Docs for This Template
-                </a>
+                <a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a>
             </li>
         </ul>
         <h2>Ecosystem</h2>
         <ul>
             <li>
-                <a
-                    href="http://router.vuejs.org/"
-                    target="_blank"
-                >
-                    vue-router
-                </a>
+                <a href="http://router.vuejs.org/" target="_blank">vue-router</a>
             </li>
             <li>
-                <a
-                    href="http://vuex.vuejs.org/"
-                    target="_blank"
-                >
-                    vuex
-                </a>
+                <a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
             </li>
             <li>
-                <a
-                    href="http://vue-loader.vuejs.org/"
-                    target="_blank"
-                >
-                    vue-loader
-                </a>
+                <a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
             </li>
             <li>
-                <a
-                    href="https://github.com/vuejs/awesome-vue"
-                    target="_blank"
-                >
-                    awesome-vue
-                </a>
+                <a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
             </li>
         </ul>
         <router-link to="routes" class="button primary">Routes</router-link>
@@ -88,13 +41,8 @@
 </template>
 
 <script>
-import Logo from '@/assets/svg/libeo-logo.svg';
-
 export default {
     name: 'HelloWorld',
-    components: {
-        Logo,
-    },
     data() {
         return {
             msg: 'Welcome to Your Vue.js App',
@@ -105,9 +53,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-svg {
-    margin: 80px 0;
-}
 .hello {
     margin-top: 60px;
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
