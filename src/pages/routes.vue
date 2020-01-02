326e11edca6a9d5007690970533529f2616c55fa
Sébastien LeBlanc <seb110896@gmail.com>
2020-01-02 16:49:52 -0500

    Stylelint and fix

--- src/pages/routes.vue
+++ src/pages/routes.vue
@@ -40,12 +40,12 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .routes {
+    margin-top: 60px;
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
     a {
         display: block;
         color: #42b983;
