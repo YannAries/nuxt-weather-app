c2649d995be5ab4c5536dcb84d822b123e09ebbe
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2020-10-20 16:29:53 -0400

    Suppression du header & footer

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,22 +1,8 @@
 <template>
     <div class="layout">
-        <app-header />
         <nuxt />
-        <app-footer />
     </div>
 </template>
-
-<script>
-import AppHeader from "@/components/AppHeader"
-import AppFooter from "@/components/AppFooter"
-
-export default {
-    components: {
-        AppHeader,
-        AppFooter,
-    }
-}
-</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only & Pages-->
 <style lang="scss">
