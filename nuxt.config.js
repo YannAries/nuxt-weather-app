ba9833a48276eceabc807524b5bf58a3f3a93ba4
Sébastien LeBlanc <seb110896@gmail.com>
2020-10-22 17:07:41 -0400

    [FIX] Downgrade eslint-module to v2

--- nuxt.config.js
+++ nuxt.config.js
@@ -150,17 +150,6 @@ module.exports = {
                     svgo,
                 },
             });
-
-            // Lint fix on save
-            config.module.rules.push({
-                enforce: 'pre',
-                test: /\.(js|vue)$/,
-                loader: 'eslint-loader',
-                exclude: /(node_modules)/,
-                options: {
-                    fix: true,
-                },
-            });
         },
     },
 };
